9327552064c02a06cc75153427c242c47888871d
Dmitriy <z_lynx@mail.ru>
2019-02-15 17:57:18 +0300

    #1 implemented

---
+++ Dmitriy/LAB_1/1_CelciusToFahrenheit/CelciusToFahrenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CelciusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tCelcius;
+            double tFahrenheit;
+            Console.Write("Enter Celsius:");
+            tCelcius = Convert.ToDouble(Console.ReadLine());
+            tFahrenheit = (tCelcius * 9 / 5)  + 32;
+            Console.WriteLine("Celsius temperature {0}°C equal to {1}°F in Fahrenheit.", tCelcius, tFahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
+
