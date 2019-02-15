3063cbb5ddd312c81dd786d988e62228424e79ae
Dmitriy <z_lynx@mail.ru>
2019-02-15 18:00:38 +0300

    #3 implemented

---
+++ Dmitriy/LAB_1/3_Numbers/3numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber; 
+            int secondNumber;
+            int thirdNumber;
+            Console.Write("Enter FIRST  digit: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter SECOND digit: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter THIRD  digit: ");
+            thirdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("SUMM: {0}", firstNumber + secondNumber + thirdNumber);
+            Console.WriteLine("MULTIPLY: {0}", firstNumber * secondNumber * thirdNumber);
+            Console.WriteLine("AVERAGE: {0}", (firstNumber + secondNumber + thirdNumber) / 3.0);
+            Console.ReadKey();
+        }
+    }
+}
