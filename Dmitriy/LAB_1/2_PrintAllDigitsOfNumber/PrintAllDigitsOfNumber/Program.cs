d2607e518a19f32f37dd542802247873ad26a353
Dmitriy <z_lynx@mail.ru>
2019-02-15 17:58:54 +0300

    #2 implemented

---
+++ Dmitriy/LAB_1/2_PrintAllDigitsOfNumber/PrintAllDigitsOfNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PrintAllDigitsOfNumber
+{
+    class Program
+    {
+        /* ПЕРВЫЙ ВАРИАНТ ЧЕРЕЗ РЕКУРСИВНУЮ ФУНКЦИЮ */
+        static void PrintDigits(int value)
+        {
+            if (value >= 10)
+            {
+                PrintDigits(value / 10);
+            }
+            Console.Write("{0},", value % 10);
+        }
+        /* ПЕРВЫЙ ВАРИАНТ ЧЕРЕЗ РЕКУРСИВНУЮ ФУНКЦИЮ */
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            
+            /* ПЕРВЫЙ ВАРИАНТ ЧЕРЕЗ РЕКУРСИВНУЮ ФУНКЦИЮ */
+            PrintDigits(number);
+            /* ПЕРВЫЙ ВАРИАНТ ЧЕРЕЗ РЕКУРСИВНУЮ ФУНКЦИЮ */
+
+            /* ВТОРОЙ ВАРИАНТ ЧЕРЕЗ ЦИКЛ*/
+            Console.WriteLine();
+            string resultString = "";
+            while (number > 0)
+            {
+                resultString = Convert.ToString(number % 10) + "," + resultString;
+                number = number / 10;
+            }
+            Console.Write(resultString);
+            /* ВТОРОЙ ВАРИАНТ ЧЕРЕЗ ЦИКЛ */
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dmitriy/LAB_1/2_PrintAllDigitsOfNumber/PrintAllDigitsOfNumber/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PrintAllDigitsOfNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrintAllDigitsOfNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7332d7c7-3651-40bf-aff9-ef6ed30cb90e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
